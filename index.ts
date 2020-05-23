bfa2a3f37569fee2c4f9454e5900989a6ee516d7
meeroslav <missing.manual@gmail.com>
2020-05-23 23:29:12 +0200

    feat: add toggle button

--- index.ts
+++ index.ts
@@ -2,5 +2,19 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TS-809</h1>`;
+// const appDiv: HTMLElement = document.getElementById('app');
+// appDiv.innerHTML = `
+//   <h1>TS-809</h1>
+//   <button type="button" onclick="togglePlay()">Toggle</button>
+// `;
+let playState = false;
+const toggleBtn: HTMLElement = document.getElementById('toggle');
+toggleBtn.innerHTML = 'Play';
+
+const togglePlay = () => {
+  playState = !playState;
+  toggleBtn.innerHTML = playState ? 'Stop' : 'Play';
+}
+
+// make it globally accessible
+window.togglePlay = togglePlay;
