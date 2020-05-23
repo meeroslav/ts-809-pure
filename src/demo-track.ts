7a52011ac89ea7519df470bb1b3b24ee3bdddb83
Miroslav Jonas <missing.manual@gmail.com>
2020-05-24 00:20:14 +0200

    feat: add samples and demo rhythm

---
+++ src/demo-track.ts
@@ -0,0 +1,8 @@
+import { Tracks } from './models/track';
+
+export const demoRhythm: Tracks = [
+  ['Urban_Kick_FAT.wav',  1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+  ['909_Snare_FAT.wav',   0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  ['909_Closed_FAT.wav',  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+  ['909_Open_FAT.wav',    0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+];
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import { Tracks } from './models/track';
-
-// Write TypeScript code!
-// const appDiv: HTMLElement = document.getElementById('app');
-// appDiv.innerHTML = `
-//   <h1>TS-809</h1>
-//   <button type="button" onclick="togglePlay()">Toggle</button>
-// `;
 let playState = false;
 const toggleBtn: HTMLElement = document.getElementById('toggle') || new HTMLElement();
 toggleBtn.innerHTML = 'Play';
-
-const tracks: Tracks = [
-  ['samples', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
 
 const togglePlay = () => {
   playState = !playState;
